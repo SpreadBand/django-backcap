7bf0f6e1d833493b7fcb9dc285493a5c9955e458
Guillaume Libersat <guillaume@spreadband.com>
2011-10-28 19:15:05 +0200

    add trailing slash to urls in case of APPEND_SLASH = True

--- backcap/urls.py
+++ backcap/urls.py
@@ -26,20 +26,20 @@ urlpatterns = patterns('django.views.generic.simple',
 )
 
 urlpatterns += patterns('',             
-    url(r'^new$', views.feedback_new, name='feedback-new'),
-    url(r'^list$', views.feedback_list, name='feedback-list'),
+    url(r'^new/$', views.feedback_new, name='feedback-new'),
+    url(r'^list/$', views.feedback_list, name='feedback-list'),
     url(r'^list/(?P<qtype>\w+)$', views.feedback_list, name='feedback-list'),
     url(r'^search$', views.feedback_search, name='feedback-search'),
     url(r'^(?P<feedback_id>\d+)/$', views.feedback_detail, name='feedback-detail'),
-    url(r'^(?P<feedback_id>\d+)/update$', views.feedback_update, name='feedback-update'),
-    url(r'^(?P<feedback_id>\d+)/close$', views.feedback_close, name='feedback-close'),
-    url(r'^(?P<feedback_id>\d+)/ping$', views.feedback_ping_observers, name='feedback-ping-observers'),
+    url(r'^(?P<feedback_id>\d+)/update/$', views.feedback_update, name='feedback-update'),
+    url(r'^(?P<feedback_id>\d+)/close/$', views.feedback_close, name='feedback-close'),
+    url(r'^(?P<feedback_id>\d+)/ping/$', views.feedback_ping_observers, name='feedback-ping-observers'),
 
     # Following
-    url(r'^(?P<feedback_id>\d+)/follow$', views.feedback_follow, name='feedback-follow'),
-    url(r'^(?P<feedback_id>\d+)/unfollow$', views.feedback_unfollow, name='feedback-unfollow'),
+    url(r'^(?P<feedback_id>\d+)/follow/$', views.feedback_follow, name='feedback-follow'),
+    url(r'^(?P<feedback_id>\d+)/unfollow/$', views.feedback_unfollow, name='feedback-unfollow'),
 
-    url(r'^feedback-tab$', views.feedback_tab, name='feedback-tab'),
+    url(r'^feedback-tab/$', views.feedback_tab, name='feedback-tab'),
 
     # Votes
     url(r'^(?P<feedback_id>\d+)/(?P<direction>up|down|clear)vote/?$', views.feedback_vote, name='feedback-vote'),
